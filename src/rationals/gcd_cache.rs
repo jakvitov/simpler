9d98c8a5d8c8bdaa6f355646d1158f10046b380f
Jakub Vítovec <jakvitov@gmail.com>
2025-08-17 22:04:28 +0200

    Lint hints

--- src/rationals/gcd_cache.rs
+++ src/rationals/gcd_cache.rs
@@ -8,27 +8,27 @@ pub struct GcdCache {
 
 //Gcd implemented using the Eucleidian algorithm
 fn gcd_eucleidian(mut a: i128, mut b: i128) -> Result<i128, Box<NumericalError>> {
-    if (a == b && b == 0) {
-        return Result::Err(Box::new(NumericalError::new("Both arguments of gcd are zero.", format!("gcd({}, {})", a,b))));
-    } else if (a < 0 || b < 0) {
-        return Result::Err(Box::new(NumericalError::new("Some of the gcd arguments are smaller than zero", format!("lcm({},{})", a,b))));
+    if a == b && b == 0 {
+        return Err(Box::new(NumericalError::new("Both arguments of gcd are zero.", format!("gcd({}, {})", a,b))));
+    } else if a < 0 || b < 0 {
+        return Err(Box::new(NumericalError::new("Some of the gcd arguments are smaller than zero", format!("lcm({},{})", a,b))));
     }
     while b != 0 {
         let t = b;
         b = a % b;
         a = t;
     }
-    return Result::Ok(a);
+    Ok(a)
 }
 
 fn lcm_eucleidian(a: i128, b: i128, gcd_cache: &mut GcdCache) -> Result<i128, Box<NumericalError>> {
-    if (a == 0 || b == 0) {
+    if a == 0 || b == 0 {
         return Result::Err(Box::new(NumericalError::new("Both arguments of LCM are zero.", format!("lcm({},{})", a,b))));
-    } else if (a < 0 || b < 0) {
+    } else if a < 0 || b < 0 {
         return Result::Err(Box::new(NumericalError::new("Either of the arguments of LCM are negative.", format!("lcm({},{})", a,b))));
     }
     let gcd = gcd_cache.gcd(a,b)?;
-    return Result::Ok((a*b) /gcd);
+    Ok((a*b) /gcd)
 }
 
 impl GcdCache {
@@ -39,22 +39,21 @@ impl GcdCache {
 
 
     pub fn gcd(&mut self, a: i128, b: i128) -> Result<i128, Box<NumericalError>> {
-        let (bigger, smaller) = if (a > b) {(a, b)} else {(b, a)};
+        let (bigger, smaller) = if a > b {(a, b)} else {(b, a)};
 
-        return match self.data.get(&(bigger, smaller)) {
-            Some(res) => Result::Ok({*res} ),
+        match self.data.get(&(bigger, smaller)) {
+            Some(res) => Result::Ok(*res),
             None => {
                 let gcd = gcd_eucleidian(bigger, smaller)?;
                 self.data.insert((bigger, smaller), gcd );
-                return Result::Ok(gcd);
+                Ok(gcd)
             }
         }
     }
 
     pub fn lcm(&mut self, a: i128, b: i128) -> Result<i128, Box<NumericalError>> {
-        let (bigger, smaller) = if (a > b) {(a, b)} else {(b, a)};
         let res = lcm_eucleidian(a, b, self)?;
-        return Result::Ok(res);
+        Ok(res)
     }
 
 }
@@ -84,7 +83,7 @@ mod tests {
         let a = 0;
         let b = 10;
         let res = cache.gcd(a,b);
-        assert!(res.is_err() == false);
+        assert!(!res.is_err());
 
         let Ok(result) = res else {
             panic!("Result is err");
