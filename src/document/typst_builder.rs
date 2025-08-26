93d3a1de73f0ebe434454494a5fa9e845158bfac
Jakub Vítovec <jakvitov@gmail.com>
2025-08-26 14:44:09 +0200

    Move logic for TypstDocument methods

--- src/document/typst_builder.rs
+++ src/document/typst_builder.rs
@@ -10,27 +10,30 @@ pub struct TypstDocument {
 }
 
 impl TypstDocument {
-    pub fn empty() -> Self {
-        return TypstDocument {data: String::new()};
-    }
-
-    pub fn create_header() -> String {
-        format!(r#"
+    pub fn init() -> Self {
+        return TypstDocument {data: format!(r#"
             #set page(header: [
             _Simpler output. Version {}_
             #h(1fr)
             Generated: {}
            ])
-        "#,VERSION, Utc::now().to_string())
+        "#,VERSION, Utc::now().to_string())};
     }
 
-    pub fn export_to_typst_source(&self) -> String {
-        Self::create_header() + &self.data
+    pub fn add_header(mut self, header: &str) -> Self {
+        self.data.push_str("\n= ");
+        self.data.push_str(header);
+        self.data.push('\n');
+        self
+    }
+
+    pub fn export_to_typst_source(self) -> String {
+        self.data
     }
 
     /// Generate Pdf from given document
-    pub fn export_to_pdf(&self) -> Result<Vec<u8>, Box<PdfGenerationError>> {
-        let world = TypstWrapperWorld::new("".to_owned(), self.export_to_typst_source());
+    pub fn export_to_pdf(self) -> Result<Vec<u8>, Box<PdfGenerationError>> {
+        let world = TypstWrapperWorld::new("".to_owned(), self.data);
         let document_compilation_result =  typst::compile(&world).output;
         let document = match document_compilation_result {
             Ok(doc) => doc,
@@ -54,6 +57,22 @@ impl TypstDocument {
 
 #[cfg(test)]
 mod tests {
+    use crate::document::typst_builder::TypstDocument;
 
+    #[test]
+    fn typst_document_builder_init_writes_intro() {
+        let doc = TypstDocument::init();
+        let typst_source = doc.export_to_typst_source();
+        assert!(typst_source.contains("#set page(header: [
+            _Simpler output. Version "));
+        assert!(typst_source.contains("Generated: "));
+    }
+    
+    #[test]
+    fn typst_document_builder_add_header_succeeds() {
+        let doc = TypstDocument::init().add_header("MyHeader");
+        let res = doc.export_to_typst_source();
+        
+    }
 
 }
--- tests/document.rs
+++ tests/document.rs
@@ -5,13 +5,13 @@ use std::io::Write;
 // !Integration tests for Document building module!
 
 //Turn on to generate result pdfs for visual testing
-const WRITE_OUTPUTS_TO_PDF: bool = false;
+const WRITE_OUTPUTS_TO_PDF: bool = true;
 
 #[test]
 fn generate_simple_pdf_succeeds() {
 
 
-    let document = TypstDocument::empty();
+    let document = TypstDocument::init().add_header("ahoj");
     let pdf = document.export_to_pdf();
     assert!(pdf.is_ok());
     if WRITE_OUTPUTS_TO_PDF {
