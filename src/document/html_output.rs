bcdb77b78a875be17a1f3ca4d0fbc62cab383e98
Jakub Vítovec <jakvitov@gmail.com>
2025-09-15 22:43:17 +0200

    Bounds added to html output

--- src/document/html_output.rs
+++ src/document/html_output.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 use chrono::Utc;
-use crate::parsers::mps::MpsModel;
+use crate::parsers::mps::{Constraints, MpsModel};
 use crate::rationals::Rational;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -64,15 +64,16 @@ impl HtmlOutput {
         self.data.push_str("<div class=\"parsed_mps_model\">\n");
         self.data.push_str(format!("<h2>MPS model {}</h2>\n", mps_model.name).as_str());
         self.data.push_str(format!("<p>Rhs order: {}</p>\n", mps_model.rhs.rhs.keys().map(|x| x.to_owned()).collect::<Vec<String>>().join("|")).as_str());
-
+        self.data.push_str("<h3>Rows:</h3>\n");
         for (row_name, constraint) in &mps_model.rows.rows {
             self.data.push_str("<p>\n");
-            self.data.push_str(format!("{}:\n", &row_name).as_str());
+            self.data.push_str(format!("<b>{}:</b>\n", &row_name).as_str());
             self.data.push_str("<math display=\"block\">\n");
             self.data.push_str("<mrow>\n");
             for (variable_name, variable_values) in &mps_model.columns.variables {
                 let variable_value = variable_values.get(row_name).map_or(Rational::zero(), |x| x.to_owned());
                 self.data.push_str(variable_value.to_mmdn_with_sign().as_str());
+                self.data.push_str("<mo>×</mo>");
                 self.data.push_str(format!("<mi>{}</mi>", variable_name).as_str());
             }
             self.data.push_str(format!("<mo>{}</mo>", constraint.to_sign()).as_str());
@@ -80,13 +81,37 @@ impl HtmlOutput {
                 if let Some(rhs_value) =  rhs_values_for_rows.get(row_name) {
                     self.data.push_str(rhs_value.to_mmdn_with_sign().as_str());
 
-                } else {
+                }
+                else if *constraint == Constraints::N {
+                    self.data.push_str("<mi>objective</mi>");
+                }
+                else {
                     self.data.push_str("<mi>missing value</mi>");
                 }
             }
             self.data.push_str("</mrow>\n");
             self.data.push_str("</math>\n");
             self.data.push_str("</p>\n")
+        }
+
+        //Adding bounds to the output
+        self.data.push_str("<h3>Bounds:</h3>\n");
+        for (bound_name, bound_values) in &mps_model.bounds.bounds {
+            self.data.push_str("<p>\n");
+            self.data.push_str(format!("<b>{}:</b>\n", &bound_name).as_str());
+            for (variable_name, value, bound_type) in bound_values {
+                self.data.push_str("<p>");
+                self.data.push_str("<math display=\"block\">\n");
+                self.data.push_str("<mrow>");
+
+                self.data.push_str(format!("<mi>{variable_name}</mi>").as_str());
+                self.data.push_str(format!("<mo>{}</mo>", bound_type.to_sign()).as_str());
+                self.data.push_str(value.to_mmdn_with_sign().as_str());
+                self.data.push_str("</mrow>");
+                self.data.push_str("</math>\n");
+                self.data.push_str("</p>");
+            }
+            self.data.push_str("</p>\n");
         }
 
         self.data.push_str("</div>\n");
