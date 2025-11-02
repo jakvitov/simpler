f4b3bdc8c8e097b8c7a5c3ca1b77e711554c60be
Jakub Vítovec <jakvitov@gmail.com>
2025-11-02 21:50:21 +0100

    Matrix mul with tests

--- src/rationals/rational_matrix.rs
+++ src/rationals/rational_matrix.rs
@@ -19,6 +19,21 @@ impl RationalMatrix {
         RationalMatrix{data}
     }
 
+    pub fn from_rows(data: Vec<Vec<Rational>>) -> Option<RationalMatrix> {
+        if data.is_empty() || data.len() == 1 {
+            return Some(RationalMatrix { data })
+        }
+        if data.len() > 1 {
+            let len = data[0].len();
+            for i in &data {
+                if i.len() != len {
+                    return None;
+                }
+            }
+        }
+        Some(RationalMatrix {data})
+    }
+
     pub fn get_row(&self, i: usize) -> &Vec<Rational> {
         &self.data[i]
     }
@@ -71,11 +86,59 @@ mod tests {
         assert_eq!(*a.get_row(1), vec![Rational::zero(), Rational::zero()]);
     }
 
+    #[test]
+    fn matrix_from_empty_rows_succeeds() {
+        let a = RationalMatrix::from_rows(vec![]);
+        assert!(a.is_some());
+    }
+
+    #[test]
+    fn matrix_from_diff_length_rows_succeeds(){
+        let mut rows = Vec::new();
+        rows.push(vec![Rational::zero(), Rational::zero()]);
+        rows.push(vec![Rational::zero()]);
+        let a = RationalMatrix::from_rows(rows);
+        assert!(a.is_none());
+    }
    #[test]
    fn vector_multiplication_succeeds() {
        let mut gcd_cache = GcdCache::init();
        let a = RationalMatrix::from_value(1,2, Rational::from_integer(2));
        let b = RationalMatrix::from_value(2,1, Rational::from_integer(1));
        assert_eq!(RationalMatrix::mul(&a,&b, &mut gcd_cache).expect("Error"), RationalMatrix::from_value(1,1, Rational::from_integer(4)));
+   }
+
+   #[test]
+   fn matrix_multiplication_succeeds() {
+       let mut gcd_cache = GcdCache::init();
+       let mut a_rows = Vec::with_capacity(2);
+       a_rows.push(vec![Rational::from_integer(1), Rational::from_integer(2)]);
+       a_rows.push(vec![Rational::from_integer(2), Rational::from_integer(3)]);
+       let a = RationalMatrix::from_rows(a_rows);
+       assert!(a.is_some());
+       let a = a.unwrap();
+
+       let mut b_rows = Vec::with_capacity(2);
+       b_rows.push(vec![Rational::from_integer(1), Rational::from_integer(2), Rational::from_integer(3)]);
+       b_rows.push(vec![Rational::from_integer(3), Rational::from_integer(2), Rational::from_integer(5)]);
+
+       let b = RationalMatrix::from_rows(b_rows);
+       assert!(b.is_some());
+       let b = b.unwrap();
+
+       let c = RationalMatrix::mul(&a, &b, &mut gcd_cache);
+       assert!(c.is_ok());
+
+       let c = c.unwrap();
+
+       let mut d_rows = Vec::with_capacity(2);
+       d_rows.push(vec![Rational::from_integer(7), Rational::from_integer(6), Rational::from_integer(13)]);
+       d_rows.push(vec![Rational::from_integer(11), Rational::from_integer(10), Rational::from_integer(21)]);
+
+       let d = RationalMatrix::from_rows(d_rows);
+       assert!(d.is_some());
+       let d = d.unwrap();
+
+       assert_eq!(c, d);
    }
 }
