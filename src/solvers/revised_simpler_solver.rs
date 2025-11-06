ec6074b8247b252677fd42b337083a92c61ba3c6
Jakub Vítovec <jakvitov@gmail.com>
2025-11-06 11:34:41 +0100

    get_basis_split_cost_vector

--- src/solvers/revised_simpler_solver.rs
+++ src/solvers/revised_simpler_solver.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use crate::document::html_convertible_error::HtmlConvertibleError;
 use crate::document::html_output::HtmlOutput;
 use crate::rationals::{GcdCache, Rational, RationalMatrix};
@@ -7,10 +8,50 @@ use crate::utils::ApplicationError;
 
 pub fn solve_revised_simplex(initial_simplex_table: &BasicSimplexTable, gcd_cache: &mut GcdCache, html_output: &mut HtmlOutput) -> Result<Option<Rational>, Box<dyn HtmlConvertibleError>> {
     html_output.add_simplex_solver_header(REVISED_SIMPLEX);
+
+    let mut base_variables: Vec<String> = initial_simplex_table.base_variable_names.clone();
+    let basis_matrix = get_basis_matrix(&base_variables, initial_simplex_table).map_err(|x| x as Box<dyn HtmlConvertibleError>)?;
+    let basis_inverse = basis_matrix.inverse(gcd_cache).map_err(|x| x as Box<dyn HtmlConvertibleError>)?;
+
+    if basis_inverse.is_none() {
+        return Err(Box::new(ApplicationError::from_string_details("Singular basis matrix encountered.", format!("Basis matrix: {:?}", basis_matrix))));
+    }
+
+
+
     Ok(None)
+}
+
+/// Return pair of basic and non-basic cost vectors
+/// Return (c_b, c_nb)
+fn get_basis_split_cost_vector(initial_simplex_table: &BasicSimplexTable, basic_variable_names: &Vec<String>) ->  Result<(Vec<Rational>, Vec<Rational>), Box<ApplicationError>> {
+    let mut basic_cost_vector: Vec<Rational> = Vec::with_capacity(basic_variable_names.len());
+    let mut non_basic_cost_vector: Vec<Rational> = Vec::with_capacity(initial_simplex_table.rows.len() - basic_variable_names.len());
+
+    let Some(first) = initial_simplex_table.rows.first() else {
+        return Ok((basic_cost_vector, non_basic_cost_vector));
+    };
+
+    let mut basic_variable_indexes = HashSet::new();
 
 
+    for basic_variable_name in  basic_variable_names {
+        let basic_variable_index = initial_simplex_table.column_variable_names.get(basic_variable_name);
+        let Some(basic_variable_index) =  basic_variable_index else {
+            return Err(Box::new(ApplicationError::from_string_details("Basic variable not found in initial simplex table.", format!("Basic variable {}. Available variables {:?}", basic_variable_name, initial_simplex_table.column_variable_names.keys().collect::<Vec<_>>()))));
+        };
+        basic_variable_indexes.insert(basic_variable_index);
+    }
 
+    for i in 0..first.len() {
+        if basic_variable_indexes.contains(&i) {
+            basic_cost_vector.push(initial_simplex_table.objective_row[i]);
+        } else {
+            non_basic_cost_vector.push(initial_simplex_table.objective_row[i]);
+        }
+    }
+
+    Ok((basic_cost_vector, non_basic_cost_vector))
 }
 
 /// Get basis matrix for basic variables
@@ -43,7 +84,7 @@ fn get_basis_matrix(basic_variables: &Vec<String>, initial_simplex_table: &Basic
 mod tests {
     use crate::rationals::Rational;
     use crate::solvers::basic_simplex_table_data::test_utils::create_minimal_simplex_table_for_testing;
-    use crate::solvers::revised_simpler_solver::get_basis_matrix;
+    use crate::solvers::revised_simpler_solver::{get_basis_matrix, get_basis_split_cost_vector};
 
     #[test]
     fn get_basis_matrix_succeeds() {
@@ -62,5 +103,12 @@ mod tests {
 
     }
 
+    #[test]
+    fn get_basis_split_cost_vector_succeeds() {
+        let simplex_table = create_minimal_simplex_table_for_testing();
+        let (c_b, c_nb) = get_basis_split_cost_vector(&simplex_table, &vec!["x1".to_owned(), "S2".to_owned()]).expect("Basis vector should be correct");
 
+        assert_eq!(c_b, vec![Rational::from_integer(-1), Rational::zero()]);
+        assert_eq!(c_nb, vec![Rational::from_integer(-2), Rational::zero()]);
+    }
 }
