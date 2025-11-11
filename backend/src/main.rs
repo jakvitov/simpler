b11413aaffa0eeabd4f27f9d774fd4b82969c715
Jakub Vítovec <jakvitov@gmail.com>
2025-11-11 23:24:57 +0100

    Setup of async http server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,26 @@
 extern crate core;
 
+use crate::server::health_check;
+use axum::routing::get;
+use axum::Router;
+use serde::Serialize;
+
 mod parsers;
 mod rationals;
 mod utils;
 mod document;
 mod solvers;
+mod dto;
+mod server;
 
-fn main() {
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/api/health", get(health_check));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ backend/src/server/mod.rs
@@ -0,0 +1,3 @@
+mod manage;
+
+pub use manage::health_check;
