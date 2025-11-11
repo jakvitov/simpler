aa5e00f971e24a88d445dfa6e1f33ffe38e2ce61
Jakub Vítovec <jakvitov@gmail.com>
2025-11-11 22:14:09 +0100

    Rational matrix multiplication dimension bug fix

--- backend/src/utils/tests.rs
+++ backend/src/utils/tests.rs
@@ -8,7 +8,7 @@ pub enum CorrectMps {
 
 pub fn setup_path_to_mps(mps_name: &str, correct_mps: CorrectMps) -> PathBuf {
         let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        path.push("tests/data/mps");
+        path.push("../../tests/data/mps");
         match correct_mps {
                 CorrectMps::CORRECT => path.push("correct"),
                 CorrectMps::INCORRECT => path.push("incorrect"),
