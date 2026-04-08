5dd38f7668d9a8e32c670107a1b9ac8395d23999
Jakub Vítovec <jakvitov@gmail.com>
2026-04-08 20:08:59 +0200

    FE - Revised simplex api and types

---
+++ frontend/src/api/solver/revised/revisedSimplexSolveTypes.ts
@@ -0,0 +1,40 @@
+import type {SolutionStatus} from "../solveLpTypes.ts";
+import type {SimplexTable} from "../../common/lpDefinitionTypes.ts";
+import type {TwoPhaseSimplexObjectiveRowNormalizationDto} from "../two-phase/twoPhaseSimplexSolveTypes.ts";
+import type {Rational} from "../../common/math.ts";
+
+export interface RevisedSimplexIterationDto {
+    currentBasis: String[],
+    initialBasisMatrix: Rational[][],
+    initialBasisMatrixInverse: Rational[][],
+    xB: Rational[][],
+    originalSimplexTableReducedCosts: Rational[][],
+    yT: Rational[][],
+    nonBasicVariablesCurrentReducedCosts: Rational[][],
+    enteringVariableIndex: number|undefined,
+    directionVector: Rational[][]|undefined,
+    ratioVector: Rational[]|undefined,
+    leavingVariableIndex: number|undefined,
+    updatedBasis: string[]|undefined
+}
+
+export interface RevisedSimplexPhaseOneSolutionDto {
+    initialSimplexTable: SimplexTable;
+    artificialVariablesNormalization: TwoPhaseSimplexObjectiveRowNormalizationDto;
+    iterations: RevisedSimplexIterationDto,
+    resultBase: string[]|undefined
+}
+
+export interface RevisedSimplexPhaseTwoSolutionDto {
+    initialFeasibleBase: string[]|undefined,
+    iterations: RevisedSimplexIterationDto[],
+}
+
+export interface SolveLpRevisedSimlexResponseDto {
+    solutionStatus: SolutionStatus;
+    initialSimplexTable: SimplexTable;
+    revisedSimplexPhaseOneSolution: RevisedSimplexPhaseOneSolutionDto|undefined;
+    resultVariableValues: Record<string, Rational>|undefined;
+    solutionObjectiveFunctionValue: Rational|undefined;
+    success: boolean
+}
