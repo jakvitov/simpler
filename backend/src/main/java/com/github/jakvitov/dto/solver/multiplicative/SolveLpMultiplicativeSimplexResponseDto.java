a91c796a7d7e93fd9005c48b99f4e49aca05d91a
Jakub Vítovec <jakvitov@gmail.com>
2026-04-14 16:19:14 +0200

    BE - MultiplicativeSimplexSolverService simplex controller concept

---
+++ backend/src/main/java/com/github/jakvitov/dto/solver/multiplicative/SolveLpMultiplicativeSimplexResponseDto.java
@@ -0,0 +1,9 @@
+package com.github.jakvitov.dto.solver.multiplicative;
+
+import io.micronaut.serde.annotation.Serdeable;
+import lombok.Data;
+
+@Data
+@Serdeable
+public class SolveLpMultiplicativeSimplexResponseDto {
+}
---
+++ backend/src/main/java/com/github/jakvitov/service/MultiplicativeSimplexSolverService.java
@@ -0,0 +1,14 @@
+package com.github.jakvitov.service;
+
+import com.github.jakvitov.dto.solver.SolveLpRequestDto;
+import com.github.jakvitov.dto.solver.multiplicative.SolveLpMultiplicativeSimplexResponseDto;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class MultiplicativeSimplexSolverService {
+
+    public SolveLpMultiplicativeSimplexResponseDto handleSolveTwoPhaseSimplexRequest(SolveLpRequestDto solveLpRequestDto) {
+        return null;
+    }
+
+}
