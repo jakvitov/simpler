a15dc7fb985892c362343b67e767bcfd06126bb5
Jakub Vítovec <jakvitov@gmail.com>
2026-04-15 18:14:00 +0200

    BE - Startup banner and message configured

--- backend/src/main/java/com/github/jakvitov/Application.java
+++ backend/src/main/java/com/github/jakvitov/Application.java
@@ -1,10 +1,28 @@
 package com.github.jakvitov;
 
 import io.micronaut.runtime.Micronaut;
+import lombok.extern.slf4j.Slf4j;
 
+import java.awt.*;
+import java.net.URI;
+
+@Slf4j
 public class Application {
+
+    private final static String APPLICATION_URL = "http://localhost:8080/";
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+        startBrowserWindowOrShowRef();
+    }
+
+    private static void startBrowserWindowOrShowRef() {
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                desktop.browse(new URI(APPLICATION_URL));
+            }
+        } catch (Exception ignored) {}
+        log.info("Simpler successfully started up and is available at: " + APPLICATION_URL);
     }
 }
