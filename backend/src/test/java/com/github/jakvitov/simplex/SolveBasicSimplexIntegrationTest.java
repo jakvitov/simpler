333c83b785553de23a9cf25d287b13cc54d5bccf
Jakub Vítovec <jakvitov@gmail.com>
2026-04-20 18:48:25 +0200

    BE - Added integration test for basic simplex TESTMIN

--- backend/src/test/java/com/github/jakvitov/simplex/SolveBasicSimplexIntegrationTest.java
+++ backend/src/test/java/com/github/jakvitov/simplex/SolveBasicSimplexIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.github.jakvitov.simplex;
 
+import com.github.jakvitov.dto.solver.SolutionStatus;
 import com.github.jakvitov.dto.solver.SolveLpRequestDto;
 import com.github.jakvitov.dto.solver.basic.SolveLpBasicSimplexResponseDto;
 import com.github.jakvitov.service.BasicSimplexSolverService;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.hipparchus.fraction.BigFraction;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
@@ -48,33 +50,36 @@ public class SolveBasicSimplexIntegrationTest {
     }
 
     @Test
-    public void solve_basic_simplex_succeeds() {
-        String inputMps = """
-                NAME          SIMPLELP
+    public void solve_basic_simplex_minimalization_testmin_succeeds() {
+        String input = """
+                NAME          TESTMIN
                 ROWS
                  N  COST
                  L  C1
                  L  C2
                  L  C3
                 COLUMNS
-                    X1        COST      -3
-                    X1        C1         1
-                    X1        C2         1
-                    X1        C3         0
-                    X2        COST      -2
-                    X2        C1         1
-                    X2        C2         0
-                    X2        C3         1
+                    X1        COST      3
+                    X1        C1        1
+                    X1        C2        2
+                    X1        C3        1
+                    X2        COST      2
+                    X2        C1        1
+                    X2        C2        1
+                    X2        C3        3
                 RHS
-                    RHS1      C1         4
-                    RHS1      C2         2
-                    RHS1      C3         3
+                    RHS1      C1        4
+                    RHS1      C2        5
+                    RHS1      C3        7
                 ENDATA
                 """;
-
-        SolveLpRequestDto solveLpRequestDto = new SolveLpRequestDto(inputMps, OptimisationTarget.MAX, SimplexVariant.BASIC_SIMPLEX, null, null);
+        SolveLpRequestDto solveLpRequestDto = new SolveLpRequestDto(input, OptimisationTarget.MIN, SimplexVariant.BASIC_SIMPLEX, null, null);
         SolveLpBasicSimplexResponseDto response = basicSimplexSolverService.handleSolveBasicSimplexRequest(solveLpRequestDto);
-        System.out.println(response);
+        assert response.getSolutionStatus().equals(SolutionStatus.SOLVED);
+        assert response.getSolutionObjectiveFunctionValue().equals(BigFraction.ZERO);
+        assert response.getResultVariableValues().get("S_1").equals(new BigFraction(4));
+        assert response.getResultVariableValues().get("S_2").equals(new BigFraction(5));
+        assert response.getResultVariableValues().get("S_3").equals(new BigFraction(7));
     }
 
 }
